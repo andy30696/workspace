1f958082c13f7377da2558a1db4731e38b35bf00
andy30696 <andy30696@hotmail.com>
2023-08-16 10:30:57 -0300

    Comienzo parte 3

--- js/products.js
+++ js/products.js
@@ -1,3 +1,5 @@
+const div= document.getElementById("productos");
+
 const storedValue = localStorage.getItem("catID");
 const DATA_URL = `https://japceibal.github.io/emercado-api/cats_products/${storedValue}.json`;
 
@@ -5,11 +7,17 @@ const container = document.getElementById("productos");
 
 function showData(dataArray) {
   for (const item of dataArray) {
-    container.innerHTML += `<p> <img src="${item.image}"> 
-       <br> ${item.name} 
-       <br> ${item.description} 
-       <br> ${item.cost} 
-       <br> ${item.soldCount}<p>`;
+    container.innerHTML += `
+    <div class="row card" style="width: 45%">
+     <img class="card-img-top" width="200" src="${item.image}"alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">${item.name}</h5>
+        <p class="card-text">${item.description}
+        <br> Precio USD: ${item.cost} 
+        <br> Vendidos: ${item.soldCount}</p>
+        <a href="#" class="btn btn-primary">Comprar</a>
+      </div>
+    </div>`;
   }
 }
 
@@ -18,18 +26,3 @@ fetch(DATA_URL)
   .then((data) => showData(data.products))
   .catch((error) => console.error("Error al obtener los datos:", error));
 
-function showProducts(array) {
-  let txtenHTML = "";
-  array.forEach((products) => {
-    txtenHTML += `
-        <div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="${products.image}" alt="Card image cap">
-        <div class="card-body">
-            <h5 class="card-title">${products.name}</h5>
-            <p class="card-text">${products.description}</p>
-            <a href="#" class="btn btn-primary">Comprar</a>
-        </div>
-      </div>`;
-  });
-  div.innerHTML = txtenHTML;
-}
