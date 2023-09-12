d363ab2155f4368e4adcc7ad23a33d3d2d9dd674
nacho0808 <nachocarvalho0808@gmail.com>
2023-09-12 12:00:01 -0300

    Seccion comentarios

--- js/product-info.js
+++ js/product-info.js
@@ -1,5 +1,6 @@
 const storedValue = localStorage.getItem("setProduct");
 const URL_PRODUCT = `https://japceibal.github.io/emercado-api/products/${storedValue}.json`;
+const URL_COMENTARIOS = `https://japceibal.github.io/emercado-api/products_comments/${storedValue}.json`;
 const productDetailsElement = document.getElementById('product-details');
 
 
@@ -7,11 +8,12 @@ fetch(URL_PRODUCT)
   .then(response => response.json()
   )
   .then(data => {
-    console.log(data)
-    showProduct(data)
+    console.log(data);
+    showProduct(data);
   })
   .catch(error => console.error('Error al obtener información del producto:', error));
 
+/*---------------------------------Productos-----------------------------------------*/
 
 function showProduct(dataObj) {
   console.log(dataObj)
@@ -56,24 +58,110 @@ function showProduct(dataObj) {
           </div>
                     
           <div class="row">
-            <div class="col-12 col-sm-6 col-md-4 my-3">
-            <h3><b>Imagenes ilustrativas</b></h3>
-                <img src="${img1}" alt="Imagenes representativas de ${name}">
-            </div>
-            <div class="col-12 col-sm-6 col-md-4 my-3">
-                <img src="${img2}" alt="Imagenes representativas de ${name}">
-            </div>
-            <div class="col-12 col-sm-6 col-md-4 my-3">
-                <img src="${img3}" alt="Imagenes representativas de ${name}">
-            </div>
-            <div class="col-12 col-sm-6 col-md-4 my-3">
-                <img src="${img4}" alt="Imagenes representativas de ${name}">
-            </div>
+          <div>
+          <img src="${img1}" height = "195" alt="Imagenes representativas de ${name}">
+          <img src="${img2}" height = "195" alt="Imagenes representativas de ${name}">
+          <img src="${img3}" height = "195" alt="Imagenes representativas de ${name}">
+          <img src="${img4}" height = "195" alt="Imagenes representativas de ${name}">
+          </div>
          </div>
           
     </div>
     `;
   productDetailsElement.innerHTML = template;
+}
+
+/*-------------------------COMENTARIOS----------------------------------- */
+fetch(URL_COMENTARIOS)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    print_comentarios(data);
+  })
+  .catch(error => console.error('Error al obtener información del producto:', error));
+
+
+function print_comentarios(data) {
+  
+  let data_comentarios = ""
+
+  for (let i = 0; i < data.length; i++) {
+    let estrellas = data[i].score;
+    console.log("estrellas", estrellas)
+    if (estrellas == 0) {
+      data_comentarios += `
+      <div class="list-group-item"></div>
+      <p><strong>${data[i].user}</strong> - ${data[i].dateTime} - ${data[i].score} 
+      <span class="fa fa-star checked"></span>
+      <span class="fa fa-star"></span>
+      <span class="fa fa-star"></span>
+      <span class="fa fa-star"></span>
+      <span class="fa fa-star"></span></p>
+      <p>${data[i].description}</p>
+    </div>`
+    } else if (estrellas == 1) {
+      data_comentarios += `
+        <div class="list-group-item"></div>
+          <p><strong>${data[i].user}</strong> - ${data[i].dateTime} - ${data[i].score} 
+          <span class="fa fa-star checked"></span>
+          <span class="fa fa-star"></span>
+          <span class="fa fa-star"></span>
+          <span class="fa fa-star"></span>
+          <span class="fa fa-star"></span></p>
+          <p>${data[i].description}</p>
+        </div>`
+    } else if (estrellas == 2) {
+      data_comentarios += `
+      <div class="list-group-item">
+        <p><strong>${data[i].user}</strong> - ${data[i].dateTime} - ${data[i].score} 
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star"></span>
+        <span class="fa fa-star"></span>
+        <span class="fa fa-star"></span></p>
+        <p>${data[i].description}</p>
+      </div>`
+
+    } else if (estrellas == 3) {
+      data_comentarios += `
+      <div class="list-group-item">
+        <p><strong>${data[i].user}</strong> - ${data[i].dateTime} - ${data[i].score} 
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star"></span>
+        <span class="fa fa-star"></span></p>
+        <p>${data[i].description}</p>
+      </div>`
+
+    } else if (estrellas == 4) {
+      data_comentarios += `
+      <div class="list-group-item"></div>
+        <p><strong>${data[i].user}</strong> - ${data[i].dateTime} - ${data[i].score} 
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star"></span></p>
+        <p>${data[i].description}</p>
+      </div>`
+
+    } else if (estrellas == 5) {
+      data_comentarios += `
+      <div class="list-group-item">
+        <p><strong>${data[i].user}</strong> - ${data[i].dateTime} - ${data[i].score} 
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span>
+        <span class="fa fa-star checked"></span></p>
+        <pp>${data[i].description}</pp>
+      </div>`
+    }
+
+  }
+  document.getElementById("comentarios").innerHTML = data_comentarios;
+
 }
 
 
