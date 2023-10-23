67c8d0339641f21ee04e53be8bd9bf064e754c0f
EmilianoPerezO <emilianoperez6064@gmail.com>
2023-10-23 12:43:02 -0300

    entrega6 6 6

--- js/cart.js
+++ js/cart.js
@@ -1,79 +1,15 @@
 const URL = "https://japceibal.github.io/emercado-api/user_cart/25801.json";
-let subtotalGeneral = 0;;
+let subtotalGeneral = 0;
 
 fetch(URL)
   .then((res) => res.json())
   .then((data) => {
     console.log(data);
-    showProduct(data);
     showCarrito(data);
   })
   .catch((error) => console.error("Error al obtener los datos", error));
-// Parte 1 -------------------------------------------------------------------
-function showProduct(data) {
-  const tabla = document.getElementById("tablebody");
-
-  data.articles.forEach((element) => {
-    const cantidadInput = `
-        <input type="number" id="cantidad" name="cantidad" class="form-control" min="1"
-        value="${element.count}" data-unit-cost="${element.unitCost}" style="width: 4rem;"/>
-        <label class="form-label" for="typeNumber"></label>
-    `;
-
-    tabla.innerHTML += `
-            <tr>
-                <td><img src="${element.image}" width=100px alt="product"></td>
-                <td scope="row">${element.name}</td>
-                <td scope="row">${element.currency} ${element.unitCost}</td>
-                <td scope="row">${cantidadInput}</td>
-                <td scope="row"><span class="subtotal">${element.currency} <span class="subtotal-value">${element.unitCost}</span></span></td>
-
-            </tr>
-            `;
-  });
-
-  // parte 3 -------------------------------------------------------------
-  // Agregar un evento de escucha al input de cantidad para calcular el subtotal en tiempo real
-  tabla.querySelectorAll("input[name='cantidad']").forEach((input) => {
-    input.addEventListener("input", () => {
-      const cantidad = parseInt(input.value, 10);
-      const unitCost = parseFloat(input.getAttribute("data-unit-cost"));
-      const subtotal = isNaN(cantidad) ? 0 : cantidad * unitCost;
-      const subtotalElement =
-        input.parentElement.nextElementSibling.querySelector(".subtotal");
-      subtotalElement.innerHTML = `${data.articles[0].currency} ${subtotal}`;
-
-       // Visualizar subtotal general 
-    // APARECE NAN al subir la cantidad de los productos agregados antes de subir la cantidad del producto default
-    //Si se sube primero la cantidad del defaul no sale NaN 
-    //Si no se cambia la cantidad, no aparece el subtotalgeneral
-    tabla.querySelectorAll(".subtotal").forEach((subtotalElement) => {
-      subtotalGeneral += parseFloat(subtotalElement.innerHTML.replace(data.articles[0].currency, ""));
-    });
-
-    const subtotalGeneralElement = document.getElementById("costos"); 
-    subtotalGeneralElement.innerHTML = `${data.articles[0].currency} ${subtotalGeneral}`;
-
-    });
-  });
-}
 // ----------------------------------------------------------------------------------------
-/* Envio */
 
-$(document).ready(function () {
-  $("#smartwizard").smartWizard({
-    selected: 0,
-    theme: "arrows",
-    autoAdjustHeight: true,
-    transitionEffect: "fade",
-    showStepURLhash: false,
-
-    lang: {
-      next: "Siguiente", // Cambia el texto del botón "Next" a "Siguiente"
-      previous: "Anterior", // Cambia el texto del botón "Previous" a "Anterior"
-    },
-  });
-});
 
 function showCarrito(data) {
   console.log("Entra a showCarrito");
@@ -81,6 +17,19 @@ function showCarrito(data) {
   const carritoProductos =
     JSON.parse(localStorage.getItem("carritoProductos")) || [];
   const carrito = document.getElementById("tablebody");
+
+
+  const subtotalGeneralElement = document.getElementById("costos");
+
+  carritoProductos.forEach((element) => {
+    const precioNumero = parseFloat(element.precio);
+    if (!isNaN(precioNumero)) {
+      // Si el valor es un número válido, suma al subtotal general
+      subtotalGeneral += precioNumero;
+    }
+    subtotalGeneralElement.innerHTML = subtotalGeneral;
+
+  });
 
   // Muestra los productos en la tabla del carrito
   carritoProductos.forEach((element) => {
@@ -105,19 +54,16 @@ function showCarrito(data) {
         const subtotalElement =
           input.parentElement.querySelector(".subtotal-value");
         subtotalElement.innerHTML = `${data.articles[0].currency} ${subtotal}`;
-      
-    // Visualizar subtotal general 
-    // Aparece NAN al subir la cantidad de los productos agregados antes de subir la cantidad del producto default
-    //Si se sube primero la cantidad del defaul no sale NaN 
-    //Si no se cambia la cantidad, no aparece el subtotalgeneral
-    carrito.querySelectorAll(".subtotal").forEach((subtotalElement) => {
+
+        // Visualizar subtotal general 
+        carrito.querySelectorAll(".subtotal").forEach((subtotalElement) => {
           subtotalGeneral += parseFloat(subtotalElement.innerHTML.replace(data.articles[0].currency, ""));
         });
-    
-        const subtotalGeneralElement = document.getElementById("costos"); 
+
+        const subtotalGeneralElement = document.getElementById("costos");
         subtotalGeneralElement.innerHTML = `${data.articles[0].currency} ${subtotalGeneral}`;
-      
-      
+
+
       });
     });
   });
@@ -134,7 +80,7 @@ function calcularSubtotal(precio, cantidad) {
 
 //Entrega 6
 
-function mostrarCostos() {}
+function mostrarCostos() { }
 
 //ModoNocturno
 
