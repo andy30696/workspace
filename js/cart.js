4272b68acf3444cbdf43ccb906d43e5dd79b88a0
EmilianoPerezO <emilianoperez6064@gmail.com>
2023-10-25 12:10:24 -0300

    666entrega

--- js/cart.js
+++ js/cart.js
@@ -6,6 +6,7 @@ fetch(URL)
   .then((data) => {
     console.log(data);
     showCarrito(data);
+
   })
   .catch((error) => console.error("Error al obtener los datos", error));
 // ----------------------------------------------------------------------------------------
@@ -13,37 +14,25 @@ fetch(URL)
 
 function showCarrito(data) {
   console.log("Entra a showCarrito");
+
   // Obtiene los productos almacenados en localStorage
-  const carritoProductos =
-    JSON.parse(localStorage.getItem("carritoProductos")) || [];
+  const carritoProductos = JSON.parse(localStorage.getItem("carritoProductos")) || [];
   const carrito = document.getElementById("tablebody");
-
-
-  const subtotalGeneralElement = document.getElementById("costos");
-
-  carritoProductos.forEach((element) => {
-    const precioNumero = parseFloat(element.precio);
-    if (!isNaN(precioNumero)) {
-      // Si el valor es un número válido, suma al subtotal general
-      subtotalGeneral += precioNumero;
-    }
-    subtotalGeneralElement.innerHTML = subtotalGeneral;
-
-  });
 
   // Muestra los productos en la tabla del carrito
   carritoProductos.forEach((element) => {
     const row = document.createElement("tr");
     console.log(carritoProductos);
+
     row.innerHTML = `
-    <td scope="row"><img src="${element.imagen}" width=100px alt="product"></td>
-    <td scope="row">${element.producto}</td>
-    <td scope="row">${data.articles[0].currency} ${element.precio}</td>
-    <input type="number" id="cantidad" name="cantidad" class="form-control" min="1"
-      value="${element.cantidad}" data-unit-cost="${element.precio}" style="width: 4rem";/>
-      
-  <td scope="row"><span class="subtotal-value">${data.articles[0].currency}${element.precio}</span></td>
-      `;
+      <td scope="row"><img src="${element.imagen}" width=100px alt="product"></td>
+      <td scope="row">${element.producto}</td>
+      <td scope="row">${data.articles[0].currency} ${element.precio}</td>
+      <input type="number" id="cantidad" name="cantidad" class="form-control seleccionar" min="1"
+        value="${element.cantidad}" data-unit-cost="${element.precio}" style="width: 4rem";/>
+      <td scope="row"><span class="subtotal-value">${data.articles[0].currency}${element.precio}</span></td>
+    `;
+
     carrito.appendChild(row);
 
     carrito.querySelectorAll("input[name='cantidad']").forEach((input) => {
@@ -51,36 +40,170 @@ function showCarrito(data) {
         const cantidadInput = parseInt(input.value, 10);
         const unitCost = parseFloat(input.getAttribute("data-unit-cost"));
         const subtotal = isNaN(cantidadInput) ? 0 : cantidadInput * unitCost;
-        const subtotalElement =
-          input.parentElement.querySelector(".subtotal-value");
+        const subtotalElement = input.parentElement.querySelector(".subtotal-value");
         subtotalElement.innerHTML = `${data.articles[0].currency} ${subtotal}`;
-
-        // Visualizar subtotal general 
-        carrito.querySelectorAll(".subtotal").forEach((subtotalElement) => {
-          subtotalGeneral += parseFloat(subtotalElement.innerHTML.replace(data.articles[0].currency, ""));
-        });
-
-        const subtotalGeneralElement = document.getElementById("costos");
-        subtotalGeneralElement.innerHTML = `${data.articles[0].currency} ${subtotalGeneral}`;
-
-
       });
     });
   });
+
+  // Obtén una referencia a los elementos relevantes
+  const numbers = document.querySelectorAll('.seleccionar');
+  const totalElement = document.getElementById('costos');
+  const envioInput = document.querySelectorAll("input[name='envio']");
+
+  // Función para actualizar el sub-total
+  function actualizarSubtotal() {
+    let subtotal = 0;
+    numbers.forEach(number => {
+      const cantidadInput = number.value;
+      const unitCost = parseFloat(number.getAttribute('data-unit-cost'));
+      subtotal += cantidadInput * unitCost;
+    });
+    totalElement.textContent = `Precio: ${data.articles[0].currency} ${subtotal.toFixed(2)}`;
+
+    // actualizarCostoTotal();
+    // Calcular el costo de envío
+    const costoEnvioElement = document.getElementById("costoEnvio");
+
+    // let costoEnvio = 0;
+
+    function actualizarCostoEnvio() {
+
+      const envioSeleccionado = document.querySelector("input[name='envio']:checked");
+      if (envioSeleccionado) {
+        const envioTipo = envioSeleccionado.value;
+        if (envioTipo === 'premium') {
+          costoEnvio = subtotal * 0.15;
+        } else if (envioTipo === 'express') {
+          costoEnvio = subtotal * 0.07;
+        } else if (envioTipo === 'standard') {
+          costoEnvio = subtotal * 0.05;
+        }
+      }
+      costoEnvioElement.textContent = `Precio: ${data.articles[0].currency} ${costoEnvio.toFixed(2)}`;
+
+      //actualizarCostoTotal();
+    }
+
+    function actualizarCostoTotal() {
+      const costoTotalDiv = document.getElementById("costoTotal");
+      let costoTotal = 0;
+      //costoTotal = parseFloat(costoEnvioDiv.textContent);
+      costoEnvio = parseFloat(costoEnvio);
+
+      console.log("costoEnvio dentro de actualizarCostoTotal: " + costoEnvio);
+      console.log("subtotal dentro de actualizarCostoTotal: " + subtotal);
+
+      costoTotal = costoEnvio + subtotal;
+      costoTotalDiv.textContent = `Precio: ${data.articles[0].currency} ${costoTotal.toFixed(2)}`;
+
+    }
+
+    // Agrega escuchadores de eventos a los number para detectar cambios
+
+  }
+  numbers.forEach(number => {
+    number.addEventListener('input', () => {
+      actualizarSubtotal();
+      actualizarCostoTotal();
+    });
+  });
+
+  envioInput.forEach(input => {
+    input.addEventListener('input', () => {
+      actualizarCostoEnvio();
+      actualizarCostoTotal();
+    });
+  });
+  // Inicializa el sub-total
+  // actualizarSubtotal();
+  // actualizarCostoEnvio();
+  // actualizarCostoTotal();
 }
 
 
-// Llama a la función para mostrar los productos en el carrito al cargar la página
 
-let productosDatal;
 
-function calcularSubtotal(precio, cantidad) {
-  return precio * cantidad;
+
+
+
+// Mostrar el tipo de pago seleccionado del modal
+
+const metodoPagoLabel = document.getElementById('infoDelModal');
+
+function actualizarMetodoPagoLabel() {
+  if (creditoButton.checked) {
+    metodoPagoLabel.innerHTML = "Tarjeta de crédito";
+  } else if (bancariaRadio.checked) {
+    metodoPagoLabel.innerHTML = "Transferencia bancaria";
+  } else {
+    metodoPagoLabel.innerHTML = "No ha sido seleccionado";
+  }
 }
 
-//Entrega 6
 
-function mostrarCostos() { }
+//modal-----------------------------------
+
+const creditoButton = document.getElementById('credito');
+var numTarjetaInput = document.querySelector("input[name='numTarjeta']");
+var nomTarjetaInput = document.querySelector("input[name='nomTarjeta']");
+var vencimientoInput = document.querySelector("input[name='vencimiento']");
+var cuentaInput = document.querySelector("input[name='cuenta']");
+
+
+creditoButton.addEventListener("change", () => {
+
+  if (creditoButton.checked) {
+    numTarjetaInput.disabled = false;
+    nomTarjetaInput.disabled = false;
+    vencimientoInput.disabled = false;
+    cuentaInput.disabled = true; // Deshabilitar la cuenta bancaria
+  }
+
+  actualizarMetodoPagoLabel()
+
+});
+
+var bancariaRadio = document.getElementById("bancaria");
+bancariaRadio.addEventListener("change", function () {
+  if (bancariaRadio.checked) {
+    numTarjetaInput.disabled = true;
+    nomTarjetaInput.disabled = true;
+    vencimientoInput.disabled = true;
+    cuentaInput.disabled = false; // Habilitar la cuenta bancaria
+  }
+
+  actualizarMetodoPagoLabel();
+});
+
+
+
+
+//validaciones
+
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
+
+
+
 
 //ModoNocturno
 
